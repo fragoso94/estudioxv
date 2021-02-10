1ab4daa98d770b7968869690d9b24160edacef01
fragoso94 <ing.fragoso94@gmail.com>
2021-02-10 15:33:12 -0600

    se moficaron todas las vistas con los penultimos cambios del sitio

--- correo.php
+++ correo.php
@@ -7,19 +7,46 @@
             if(isset($data['asunto']) && !empty($data['asunto'])){
                 if (isset($data['mensaje']) && !empty($data['mensaje'])){
                     $nombre = $data['nombre'];
-                    $correo = 'academia.estudioxv@gmail.com';
+                    $correo = $data['correo'];
+                    $correoAdmin = 'ing.fragoso94@gmail.com';
                     $asunto = $data['asunto'];
                     $mensaje = $data['mensaje'];
-                    $cabeceras = 'From: '.$data['correo'] . "\r\n" .
-                        'Reply-To: ing.fragoso94@gmail.com' . "\r\n" .
-                        'X-Mailer: PHP/' . phpversion();
-                    mail($correo, $asunto, $mensaje, $cabeceras);
+                    $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
+                    $cabeceras .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+                    // Cabeceras adicionales
+                    $cabeceras .= 'To: <betoliraxv@gmail.com>' . "\r\n";
+                    $cabeceras .= 'From: '. $correo . "\r\n";
+                    $cabeceras .= 'Cc: daniel.fragoso94@hotmail.com' . "\r\n";
+                    $estructuraMensaje = '
+                                         <html>
+                                         <head>
+                                           <title>Estudio Quince</title>
+                                         </head>
+                                         <body>
+                                           <p>¡Notificación de la Página Web!</p>
+                                           <table>
+                                             <tr>
+                                               <th>De:</th><td>'.$nombre.'</td>
+                                             </tr>
+                                             <tr>
+                                               <th>Asunto:</th><td>'.$asunto.'</td>
+                                             </tr>
+                                            <tr>
+                                                <th>Correo/Teléfono:</th><td>'.$correo.'</td>
+                                            </tr>
+                                            <tr>
+                                                <th>Mensaje:</th><td>'.$mensaje.'</td>
+                                            </tr>
+                                           </table>
+                                         </body>
+                                         </html> ';
+                    mail($correoAdmin, $asunto, $estructuraMensaje, $cabeceras);
                 }else{ echo parsinJson(false, "El mensaje es requerido"); }
             }else{
                 echo parsinJson(false, "El asunto es requerido");
             }
         }else{
-            echo parsinJson(false, "El correo es requerido");
+            echo parsinJson(false, "El correo/teléfono es requerido");
         }
     }else{
         echo parsinJson(false, "El nombre es requerido");
